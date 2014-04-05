97f33e52cd5be663fd9c0cdc39701aa0412b9e02
Daan <daan@mostlyoriginal.net>
2014-04-05 13:39:44 +0200

    Cleanup in preparation of move to github.

--- package_desktop.bat
+++
@@ -1,4 +0,0 @@
-call gradlew desktop:distZip
-copy E:\projects\games\ns0\ns2d\desktop\build\distributions\desktop-1.0.zip G:\dropbox\Dropbox\tox\ns2d-mld50-desktop-1.0.zip
-echo "copied to dropbox"
-pause
