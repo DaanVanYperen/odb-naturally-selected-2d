a222c87203c21e6e486daaa9a9052992698fb807
DaanVanYperen <daan@mostlyoriginal.net>
2015-02-02 23:38:46 +0100

    Hack so the map renders with normals.

--- core/src/net/mostlyoriginal/ns2d/system/render/MultipassRenderBatchingSystem.java
+++ core/src/net/mostlyoriginal/ns2d/system/render/MultipassRenderBatchingSystem.java
@@ -7,9 +7,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import net.mostlyoriginal.api.system.graphics.RenderBatchingSystem;
 import net.mostlyoriginal.ns2d.G;
+import net.mostlyoriginal.ns2d.MyMapRendererImpl;
+import net.mostlyoriginal.ns2d.system.passive.AssetSystem;
+import net.mostlyoriginal.ns2d.system.passive.CameraSystem;
 import net.mostlyoriginal.ns2d.system.passive.FramebufferManager;
+import net.mostlyoriginal.ns2d.system.passive.MapSystem;
 
 /**
  * Multipass render batching system.
@@ -19,8 +25,43 @@ import net.mostlyoriginal.ns2d.system.passive.FramebufferManager;
 @Wire(injectInherited = true)
 public class MultipassRenderBatchingSystem extends RenderBatchingSystem {
 
+	private MapSystem mapSystem;
+	private CameraSystem cameraSystem;
+	private AssetSystem assetSystem;
+
+	public MyMapRendererImpl renderer;
+
 	FramebufferManager framebufferManager;
 	AnimRenderSystem animRenderSystem;
+
+	@Override
+	protected void initialize() {
+		renderer = new MyMapRendererImpl(mapSystem.map);
+	}
+
+	protected void renderMapBehind(Texture texture) {
+		for (MapLayer layer : mapSystem.map.getLayers()) {
+			if (layer.isVisible()) {
+				if (!layer.getName().equals("infront")) {
+					renderLayer((TiledMapTileLayer) layer, texture);
+				}
+			}
+		}
+	}
+	protected void renderMapInFront(Texture texture) {
+		for (MapLayer layer : mapSystem.map.getLayers()) {
+			if (layer.isVisible()) {
+				if (layer.getName().equals("infront")) {
+					renderLayer((TiledMapTileLayer) layer, texture);
+				}
+			}
+		}
+	}
+
+	private void renderLayer(final TiledMapTileLayer layer, Texture texture) {
+		renderer.setView(cameraSystem.camera);
+		renderer.renderLayer(layer, texture);
+	}
 
 	public MultipassRenderBatchingSystem() {
 	}
@@ -33,8 +74,10 @@ public class MultipassRenderBatchingSystem extends RenderBatchingSystem {
 		diffuseBuffer.begin();
 		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		renderMapBehind(assetSystem.tileset);
 		animRenderSystem.renderNormals(false);
 		super.processEntities(entities);
+		renderMapInFront(assetSystem.tileset);
 		diffuseBuffer.end();
 
 		FrameBuffer normalBuffer = framebufferManager.getFrameBuffer(G.NORMAL_FBO);
@@ -42,7 +85,9 @@ public class MultipassRenderBatchingSystem extends RenderBatchingSystem {
 		normalBuffer.begin();
 		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		renderMapBehind(assetSystem.tilesetNormal);
 		super.processEntities(entities);
+		renderMapInFront(assetSystem.tilesetNormal);
 		normalBuffer.end();
 	}
 }
