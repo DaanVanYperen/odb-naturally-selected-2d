39204203550e914032059aeacbb50dd1d8afa252
DaanVanYperen <daan@mostlyoriginal.net>
2015-02-02 22:50:54 +0100

    Two texture shading (bit of a hack, just getting started!).

--- core/src/net/mostlyoriginal/ns2d/system/render/LightRenderSystem.java
+++ core/src/net/mostlyoriginal/ns2d/system/render/LightRenderSystem.java
@@ -3,9 +3,13 @@ package net.mostlyoriginal.ns2d.system.render;
 import com.artemis.annotations.Wire;
 import com.artemis.systems.VoidEntitySystem;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import net.mostlyoriginal.ns2d.G;
+import net.mostlyoriginal.ns2d.system.passive.AssetSystem;
 import net.mostlyoriginal.ns2d.system.passive.CameraSystem;
 import net.mostlyoriginal.ns2d.system.passive.FramebufferManager;
 
@@ -19,11 +23,16 @@ public class LightRenderSystem extends VoidEntitySystem {
 
 	private SpriteBatch batch;
 	private CameraSystem cameraSystem;
+	private ShaderProgram deferredShader;
+	private AssetSystem assetSystem;
 
 	@Override
 	protected void initialize() {
 		super.initialize();
-		this.batch = new SpriteBatch(100);
+
+		deferredShader = new ShaderProgram(Gdx.files.internal("shader/deferred.vertex"), Gdx.files.internal("shader/deferred.fragment"));
+		if ( !deferredShader.isCompiled() ) throw new RuntimeException("Compilation failed." + deferredShader.getLog());
+		this.batch = new SpriteBatch(100, deferredShader);
 	}
 
 	@Override
@@ -31,14 +40,23 @@ public class LightRenderSystem extends VoidEntitySystem {
 
 		batch.setProjectionMatrix(cameraSystem.guiCamera.combined);
 		batch.begin();
-//		shimmerProgram.setUniformf("iGlobalTime", age);
-		batch.setColor(1f, 1f, 1f, 1f);
 
-		FrameBuffer diffuseBuffer = framebufferManager.getFrameBuffer(G.DIFFUSE_FBO);
 		FrameBuffer normalBuffer = framebufferManager.getFrameBuffer(G.NORMAL_FBO);
+		bindShaderToTexture("u_texture2", 1, assetSystem.tilesetNormal);
 
-		batch.draw(diffuseBuffer.getColorBufferTexture(),0,Gdx.graphics.getHeight()* CameraSystem.ZOOM, Gdx.graphics.getWidth() * CameraSystem.ZOOM, -Gdx.graphics.getHeight()* CameraSystem.ZOOM);
+		//bindShaderToTexture("u_texture", 0, diffuseBuffer.getColorBufferTexture());
 
+		Gdx.gl.glActiveTexture(GL20.GL_TEXTURE0);
+
+		batch.setColor(1f, 1f, 1f, 1f);
+		FrameBuffer diffuseBuffer = framebufferManager.getFrameBuffer(G.DIFFUSE_FBO);
+		batch.draw(diffuseBuffer.getColorBufferTexture(), 0, (int) (Gdx.graphics.getHeight() * CameraSystem.ZOOM), (int) (Gdx.graphics.getWidth() * CameraSystem.ZOOM), -(int) (Gdx.graphics.getHeight() * CameraSystem.ZOOM));
 		batch.end();
+
+	}
+
+	private void bindShaderToTexture(String parameter, int value, Texture texture) {
+		texture.bind(value);
+		deferredShader.setUniformi(parameter, value);
 	}
 }
